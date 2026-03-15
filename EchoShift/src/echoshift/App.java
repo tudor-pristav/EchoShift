2083915b9d5d90fe7c4f9232ed8da47296950fb5
Tudor-Mihai Pristav <tpristav@uwo.ca>
2026-03-15 18:56:52 -0400

    Testing Button in JavaFX

--- EchoShift/src/echoshift/App.java
+++ EchoShift/src/echoshift/App.java
@@ -1,10 +1,29 @@
 package echoshift;
-import com.google.gson.Gson;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  /*   @Override
+    public void start(Stage stage) {
+
+        Button button = new Button("Click me");
+
+        button.setOnAction(e -> {
+            button.setText("JavaFX works!");
+        });
+
+        Scene scene = new Scene(button, 300, 200);
+
+        stage.setTitle("JavaFX Test");
+        stage.setScene(scene);
+        stage.show();
+    }
+*/
     public static void main(String[] args) {
-        Gson gson = new Gson();
-        String json = gson.toJson("hello");
-        System.out.println(json);
+        //launch();
     }
 }
