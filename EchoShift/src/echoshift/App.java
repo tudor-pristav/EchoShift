fe065bbc4679b256d3dcfe8d583631729166b4cb
Matthew Michael Taylor <mtayl273@uwo.ca>
2026-04-03 16:27:55 -0400

    Added music & sound clips to project

--- EchoShift/src/echoshift/App.java
+++ EchoShift/src/echoshift/App.java
@@ -1,21 +1,19 @@
 package echoshift;
 
-import echoshift.controllers.PlayerLoginController;
+import echoshift.UI.MainMenuView;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
       @Override
     public void start(Stage stage) {
-       //MainMenuView mainMenuView = new MainMenuView();
-       // PlayerLoginView login = new PlayerLoginView();
-        //Scene scene = new Scene(mainMenuView.createMainMenu(), 1000, 700);
-
-        //new PlayerLoginController(stage,login);
-        //stage.setScene(new Scene(login.createPlayerLoginPage(), 1280, 720));
+        MainMenuView mainMenuView = new MainMenuView();
+        Scene scene = new Scene(mainMenuView.createMainMenu(), 1000, 700);
         stage.setTitle("Echo Shift");
+        stage.setScene(scene);
         stage.show();
     }
 
